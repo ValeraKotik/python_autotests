4cacf96ba750ba0a259974c98df15d98d077c948
ValeraKotik <148646955+ValeraKotik@users.noreply.github.com>
2023-11-25 00:31:28 +0300

    Add files via upload

---
+++ test/test_pokemon.py
@@ -0,0 +1,8 @@
+import requests
+import pytest
+def test_status_code():
+    response = requests.get('https://api.pokemonbattle.me:9104/trainers')
+    assert response.status_code == 200
+def test_piece_body():
+    response = requests.get('https://api.pokemonbattle.me:9104/trainers', params= {'trainer_id': 3545})
+    assert response.json()['trainer_name'] == 'Funnyfun'
